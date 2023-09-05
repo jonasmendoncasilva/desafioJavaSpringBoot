8c234d2e00fd0480dff99a8a71b420d5729805a3
Jonas Silva <jonasmendocasilva@gmail.com>
2023-09-05 14:52:45 -0400

    ~Testes HTTP~

--- Country-Project/src/main/java/com/project/exceptions/EntityNotFoundException.java
+++ Country-Project/src/main/java/com/project/exceptions/EntityNotFoundException.java
@@ -4,6 +4,6 @@ public class EntityNotFoundException extends RuntimeException{
 	private static final long serialVersionUID = 1L;
 
 	public EntityNotFoundException(Object id) {
-		super("Country não encontrado com id "+id);
+		super("Country não encontrado com id: "+id);
 	}
 }
